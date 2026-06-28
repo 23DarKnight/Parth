007bc2ad29ae5a3a07cb44fea255f471066bb7aa
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2026-06-28 20:27:35 +0530

    jaisalmer coordinates

--- download.sh
+++ download.sh
@@ -3,9 +3,9 @@ cd /usr/share/tiles/16
 export count=78
 
 export total=1628
-for y in {27679..27667}; do
+for y in {27816..27802}; do
   mkdir $y
-	for x in {45694..45699}; do
+	for x in {46063..46079}; do
 	wget https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/16/$y/$x -O /usr/share/tiles/16/$y/$x
 	sleep 0.08
 		count=$((count + 1))
